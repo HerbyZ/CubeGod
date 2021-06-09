ef10721b9b5e1411b53cdc4882d1fc3fbb246800
HerbyZ <andreyherby@gmail.com>
2021-06-09 03:42:45 +0300

    Create Logging config

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from bot import Bot
 
+import logging
 import config
+
+logging.basicConfig(
+    format='%(asctime)s %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p',
+    level=logging.INFO
+)
 
 bot = Bot(command_prefix='!')
 bot.launch(config.BOT_TOKEN)
