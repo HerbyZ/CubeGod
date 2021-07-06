10ddded8f09423297c88f80d8415676e7b4090ec
HerbyZ <andreyherby@gmail.com>
2021-07-06 19:49:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from bot import Bot
 import logging
 import config
 
-logging.basicConfig(
-    format='%(asctime)s %(message)s',
-    datefmt='%m/%d/%Y %I:%M:%S %p',
-    level=logging.INFO
-)
 
-bot = Bot(command_prefix='!')
-bot.launch(config.BOT_TOKEN)
+def main():
+    logging.basicConfig(format='%(asctime)s %(message)s',
+                        datefmt='%m/%d/%Y %I:%M:%S %p',
+                        level=logging.INFO)
+    bot = Bot(command_prefix='!')
+    bot.launch(config.BOT_TOKEN)
+
+
+if __name__ == '__main__':
+    main()
